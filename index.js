73ae6245fde2acec59346e220cafaa9d6474af4b
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-14 01:34:11 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(driversArr, name) {
+    return driversArr.filter(driversArr => driversArr.toLowerCase() === name.toLowerCase());
+
+}
+
+function fuzzyMatch(driversArr, name) {
+    return driversArr.filter(driversArr => driversArr.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+function matchName(driversObj, name) {
+    // driversObj = {name, hometown}
+    return driversObj.filter(driversObj => driversObj.name === name)
+}
